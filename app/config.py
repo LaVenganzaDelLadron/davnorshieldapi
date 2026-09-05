525036245f22823c4f32daae3154732ffdec97ea
Elton John Justaga <darkglitch5417@gmail.com>
2026-09-05 12:41:18 +0800

    fix: rotate database

--- app/config.py
+++ app/config.py
@@ -42,11 +42,11 @@ class Settings(BaseSettings):
 
     # PostgreSQL
     DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASE_HOST: str = "postgres"
-    DATABASE_PORT: int = 5432
-    DATABASE_USER: str = "postgres"
-    DATABASE_PASSWORD: str = "postgres"
-    DATABASE_NAME: str = "tagumshieldapi"
+    DATABASE_HOST = os.getenv("DATABASE_HOST")
+    DATABASE_PORT = os.getenv("DATABASE_PORT")
+    DATABASE_USER = os.getenv("DATABASE_USER")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
 
     # Firebase
     FIREBASE_PROJECT_ID = os.getenv("PROJECT_ID")
